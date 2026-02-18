ea275b1737e5410645b4a08e0f57dfb79de3f06e
SupiiX <148445324+SupiiX@users.noreply.github.com>
2026-02-18 23:32:21 +0100

    Revert "Merge pull request #4 from bercikovalik/Update-+"
    This reverts commit 7cbdefbe681dc6702ff97612266c00f379a9ecae, reversing
    changes made to 2c905407c4c4ca35c665bb2a76b502032fc86629.

--- assets/js/recruitment-banner.js
+++ assets/js/recruitment-banner.js
@@ -8,7 +8,7 @@
 
   // --- CONFIG (csak ezt kell szerkeszteni) ---
   const RECRUITMENT = {
-    enabled: true,
+    enabled: false,
     applyUrl: 'https://forms.cloud.microsoft/pages/responsepage.aspx?id=uAHd-HZyzkqqnVdn8PSlr4kxfb5v2ttAoKPhI7QECNFUNklFQVgyQzRXOTRYRTM5TldMR1U0NDJZSi4u&origin=lprLink&route=shorturl',
     infoUrl: 'tagfelvetel.html',
     period: {
